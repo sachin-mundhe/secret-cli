9ec24612ce8124c54e603d0abf248352264d842e
Sachin Mundhe <sachinmundhe77@gmail.com>
2019-10-28 14:53:36 +0530

    File version of vault

--- main.go
+++ main.go
@@ -1,26 +1,59 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"vault"
 )
 
 func main() {
-	key := "I Love My India!"
 
-	v := vault.NewVault(key)
-	err := v.Set("username", "P95046384")
+	v := vault.NewVault("ILOVEMYINDIA", "/home/gslab/Coding/goworkspace/src/abc.txt")
+	err := v.Set("Username", "P1234567890")
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatalln(err)
 	}
 
-	originalText, err := v.Get("username")
+	err = v.Set("mo-no", "1234567890")
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatalln(err)
 	}
-	fmt.Println("Original text:", originalText)
-	// encryptedText, _ := crypt.Encrypt("Hello there!", key)
-	// fmt.Println("Encrypted text:", encryptedText)
-	// originalText, _ := crypt.Decrypt(encryptedText, key)
-	// fmt.Println("Decrypted to:", originalText)
+
+	err = v.Set("personal", "blah-blah")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
+
+//***********Encode - Convert MAP to JSON*************
+// kvp := map[string]string{"name": "Searching..."}
+// kvp["f1"] = "Rocks"
+// var sb strings.Builder
+// var sb bytes.Buffer
+// enc := json.NewEncoder(&sb)
+// enc.Encode(kvp)
+// fmt.Println(sb)
+
+// // *************Writing to file*************
+// f, err := os.OpenFile("abc.txt", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0755)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// defer func() {
+// 	err := f.Close()
+// 	if err != nil {
+// 		fmt.Println("Unable to close file...")
+// 	} else {
+// 		fmt.Println("File closed!")
+// 	}
+// }()
+
+//***********Decode - Convert JSON to MAP*************
+// file, _ := os.Open("abc.txt")
+// kvp := make(map[string]string)
+// dec := json.NewDecoder(file)
+// err := dec.Decode(&kvp)
+// if err != nil {
+// 	log.Fatalln("Error occured while decoding. Error desc:", err)
+// }
+// fmt.Println(kvp)
--- vault/vault.go
+++ vault/vault.go
@@ -1,15 +1,19 @@
 package vault
 
 import (
+	"bytes"
 	"crypt"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
 )
 
 // NewVault It returns new valut
-func NewVault(encryptionKey string) Vault {
-	return Vault{
+func NewVault(encryptionKey, filePath string) *Vault {
+	return &Vault{
 		encodingKey: encryptionKey,
-		keyValues:   make(map[string]string),
+		filePath:    filePath,
 	}
 }
 
@@ -20,11 +24,67 @@ type Vault struct {
 
 	// Map to set a cipher text with some key
 	keyValues map[string]string
+
+	//To store
+	filePath string
+}
+
+func (v *Vault) setKeyValues() error {
+
+	var sb bytes.Buffer
+	enc := json.NewEncoder(&sb)
+	enc.Encode(v.keyValues)
+
+	f, err := os.OpenFile("/home/gslab/Coding/goworkspace/src/abc.txt", os.O_RDWR|os.O_CREATE, 0755)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(sb.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *Vault) loadKeyValues() error {
+
+	fileInfo, err := os.Stat(v.filePath)
+	if os.IsNotExist(err) {
+		v.keyValues = make(map[string]string)
+		return errors.New("No such directory or file found")
+	}
+
+	if fileInfo.Size() == 0 {
+		v.keyValues = make(map[string]string)
+		return errors.New("Empty file")
+	}
+
+	file, err := os.Open(v.filePath)
+	defer file.Close()
+	if err != nil {
+		v.keyValues = make(map[string]string)
+		return err
+	}
+
+	v.keyValues = make(map[string]string)
+
+	dec := json.NewDecoder(file)
+	err = dec.Decode(&v.keyValues)
+	if err != nil {
+		return errors.New(fmt.Sprintln("Error occured while decoding. Error desc:", err))
+	}
+	return nil
 }
 
 // Get Get metjod takes in key name and check whether there is cipher text with that key name.
 // And if it is present then it decrypts it into plain text and returns it back
 func (v *Vault) Get(keyName string) (string, error) {
+	err := v.loadKeyValues()
+	if err != nil {
+		return "", err
+	}
+
 	cipherText, ok := v.keyValues[keyName]
 	if !ok {
 		return "", errors.New("No secret found")
@@ -38,10 +98,16 @@ func (v *Vault) Get(keyName string) (string, error) {
 
 // Set Set method takes in key name and plain text to encode, and converts it into cipher text
 func (v *Vault) Set(keyName, textToEncode string) error {
+
+	v.loadKeyValues()
 	encryptedValue, err := crypt.Encrypt(textToEncode, v.encodingKey)
 	if err != nil {
 		return err
 	}
 	v.keyValues[keyName] = encryptedValue
+	err = v.setKeyValues()
+	if err != nil {
+		return err
+	}
 	return nil
 }
